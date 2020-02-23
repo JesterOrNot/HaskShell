63a678f847cb8fd93f59a90d2b9cfbba6bf5ba98
Sean Hellum <seanhellum45@gmail.com>
2020-02-23 22:18:56 +0000

    update prompt

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import Lib
+import System.Process
+import Control.Monad.Trans.Class
 import System.Console.Haskeline
 
 main :: IO ()
@@ -9,9 +11,11 @@ main = runInputT defaultSettings loop
    where
        loop :: InputT IO ()
        loop = do
-           minput <- getInputLine "% "
+           minput <- getInputLine "\x1b[35mHaskTerm \x1b[32mλ \x1b[36m"
            case minput of
                Nothing -> return ()
                Just "quit" -> return ()
-               Just input -> do outputStrLn $ "Input was: " ++ input
+               Just input -> do 
+                                outputStr "\x1b[m"
+                                lift (callCommand input)
                                 loop
