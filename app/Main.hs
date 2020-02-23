0c8065fba78fae058d6b68d274b00434322cbf5e
Sean Hellum <seanhellum45@gmail.com>
2020-02-23 22:38:26 +0000

    add exception handling :octocat:

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,27 @@
 module Main where
 
-import Lib
-import System.Process
-import Control.Monad.Trans.Class
-import System.Console.Haskeline
+import           Lib
+import           System.Process
+import           System.Exit
+import           Control.Monad.Trans.Class
+import           System.Console.Haskeline
 
 main :: IO ()
 
 main = runInputT defaultSettings loop
-   where
-       loop :: InputT IO ()
-       loop = do
-           minput <- getInputLine "\x1b[35mHaskTerm \x1b[32mλ \x1b[36m"
-           case minput of
-               Nothing -> return ()
-               Just "quit" -> return ()
-               Just input -> do 
-                                outputStr "\x1b[m"
-                                lift (callCommand input)
-                                loop
+ where
+  loop :: InputT IO ()
+  loop = do
+    minput <- getInputLine "\x1b[35mHaskTerm \x1b[32mλ \x1b[36m"
+    outputStr "\x1b[m"
+    case minput of
+      Nothing     -> return ()
+      Just "exit" -> return ()
+      Just input  -> do
+        lift (runCmd input)
+        loop
+
+runCmd ""    = putStr ""
+runCmd input = do
+  system input
+  putStr ""
