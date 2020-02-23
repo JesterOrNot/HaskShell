ecfe558e31a29de188cf690ae22d3354950d5f4c
Sean Hellum <seanhellum45@gmail.com>
2020-02-23 20:37:01 +0000

    Add basic shell

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
 module Main where
 
 import Lib
+import System.Console.Haskeline
 
 main :: IO ()
-main = someFunc
+
+main = runInputT defaultSettings loop
+   where
+       loop :: InputT IO ()
+       loop = do
+           minput <- getInputLine "% "
+           case minput of
+               Nothing -> return ()
+               Just "quit" -> return ()
+               Just input -> do outputStrLn $ "Input was: " ++ input
+                                loop
